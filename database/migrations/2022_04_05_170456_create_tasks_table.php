c159c308481a28aa77874514958ccda2b6c2bcf8
Egor Bezmaternykh <bea16714@gmail.com>
2022-04-05 22:21:57 +0500

    Добавлены миграции

--- database/migrations/2022_04_05_170456_create_tasks_table.php
+++ database/migrations/2022_04_05_170456_create_tasks_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->bigInteger('emoji_id')->unsigned();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->string('header');
+            $table->string('text');
+            $table->bigInteger('status_id')->unsigned();
+            $table->bigInteger('group_id')->unsigned();
+
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
